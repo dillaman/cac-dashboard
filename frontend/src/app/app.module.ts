3c3bd5ec716951332fb5f22f993e37121b53a56c
Jason Dillaman <jason.dillaman@gmail.com>
2019-01-19 09:32:28 -0500

    Initial stub project
    Signed-off-by: Jason Dillaman <jason.dillaman@gmail.com>

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { OnsenModule } from 'ngx-onsenui';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, OnsenModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
